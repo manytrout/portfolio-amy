ae36244a48a4c24d016579d564ccc3af44e830eb
amyturton <amy.grace.elizabeth@gmail.com>
2023-06-30 15:29:57 -0700

    renamed db and connected images to tiles

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,22 +1,25 @@
-import picture from "../images/flowers-in-january.jpg";
-import getPosts from "../data/coolDatabase";
+import getPosts from "../data/projectData";
 
-function Tile({slug, title, body, projectType}) {
-		return (
-			<div className={`singleTile ${slug} ${projectType}`}>
-				{/* <h2>{props.projectName}</h2> */}
-				{/* using this as a placeholder for testing. good lord react gets weird with images.  */}
-				<img className="picture" src={picture} alt="some flowers" />
-			This component <code> (Tile.js) </code>should be a cover image. also should be from the 'database' not the tile.
-				{/* It could have (or be) a link to a page with more details about the
+function Tile({slug, title, body, projectType, titleImage,titleImageAltText}) {
+	return (
+		<div className={`singleTile ${slug} ${projectType}`}>
+			{/* <h2>{props.projectName}</h2> */}
+			{/* using this as a placeholder for testing. good lord react gets weird with images.  */}
+			{/* <img className="picture" src={picture} alt="some flowers" /> */}
+			This component <code> (Tile.js) </code>should be a cover image. also
+			should be from the 'database' not the tile.
+			{/* It could have (or be) a link to a page with more details about the
 				project if that makes sense. 
 				To indicate that it's clickable it could also surge on mobile (how?)/ have a tracer around it on hover... */}
-				<ul>
-					<li>{slug}</li>
-					<li>{title}</li>
-					<li>{body}</li>
-				</ul>
-			</div>
-		);
-	}
+			{/* <ul>
+				<li>{slug}</li>
+				<li>{title}</li>
+				<li>{body}</li>
+			</ul> */}
+			<h2>{title}</h2>
+			<p>{body}</p>
+			<img className="picture" src={titleImage} alt={titleImageAltText} />
+		</div>
+	);
+}
 export default Tile;
