da2ffb21fcdbd4be5dfd843fabca103fe2531423
amyturton <amy.grace.elizabeth@gmail.com>
2023-07-09 15:22:26 -0700

    removed some comments from Footer and added some (dead) content to About. will come back to this later

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,7 @@
-// import MainNav from "./MainNav";
-// Do I need a footer? (unsure, maybe) Does it have to be a component? (probably)
-
 function Footer(props) {
 	return (
-		// check if all of this is undefined.
 		<footer className="pageFooter">
-			{/* <MainNav className="main-nav" /> */}
 			<ul>
-				{/* check if any of these are undefined */}
 				<li>{props.myName}</li>
 				<li>{props.email}</li>
 				<li>{props.details}</li>
