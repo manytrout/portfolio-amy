f4aa3aa554d1b21d6d984b83e361b0f607262aff
amyturton <amy.grace.elizabeth@gmail.com>
2023-07-14 21:09:29 -0700

    added pre-build things

--- src/App.js
+++ src/App.js
@@ -8,28 +8,30 @@ import Footer from "./components/Footer";
 import MainNav from "./components/MainNav";
 import PageProject from "./pages/PageProject";
 import ScrollToTop from "./components/ScrollToTop";
+import {APP_FOLDER_NAME} from "./globals/globalVariables";
+import {HashLink, NavHashLink} from "react-router-hash-link";
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter path="/">
-				<Header title="Amy Turton"/>
-				{/* DONE: add a check on slugs so that page not found works as it should
+			<BrowserRouter basename={`/${APP_FOLDER_NAME}`}>
+					<Header title="Amy Turton" />
+					{/* DONE: add a check on slugs so that page not found works as it should
 					TO DO: add hash link so it can be parallax */}
-				{/* <Route path="/about" element={<About />} /> */}
-				<Routes>
-					<Route path="/" element={<PageHome />} />
-					<Route path="/:slug" element={<PageProject />} />
-				</Routes>
+					{/* <Route path="/about" element={<About />} /> */}
+					<Routes>
+						<Route path="/" element={<PageHome />} />
+						<Route path="/:slug" element={<PageProject />} />
+					</Routes>
 
-				<MainNav />
-				<Footer
-					myName="Amy Turton"
-					email="real@email.com"
-					details="some stuff"
-				/>
+					<MainNav />
+					<Footer
+						myName="Amy Turton"
+						email="real@email.com"
+						details="some stuff"
+					/>
 
-				<ScrollToTop />
+					<ScrollToTop />
 			</BrowserRouter>
 		</div>
 	);
