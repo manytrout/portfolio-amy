d2f8e11b3653860446a303e8ee5a54b30971cbd9
amyturton <amy.grace.elizabeth@gmail.com>
2023-07-14 23:20:38 -0700

    ok last round before I add more content

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,13 +3,15 @@ function MainNav() {
 	return (
 		<nav role="navigation" className="site-navigation" id="site-navigation">
 			<ul className="nav-list" id="nav-list">
-				<li>
-					<NavLink to="/">Home</NavLink>
-				</li>
-				{/* TO DO: add more links once I have HashLink working */}
-				{/* <li>
-					<NavLink to="/about">About</NavLink>
-				</li> */}
+				<div className="navItemWrapper">
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					{/* <li>
+						<NavLink to="/about">About</NavLink>
+					</li> */}
+				</div>
+				{/* TO DO: add links in page once I have HashLink working */}
 			</ul>
 		</nav>
 	);
--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,13 +1,13 @@
-function PageAbout() {
-	return (
-		<div className="aboutMe">
-			<p className="personalBackground">After years of doing application support  -- for in house web apps and niche software -- I realized that I wanted to shift from supporting apps to making them. Once I started to look into jobs that I could see myself doing, front-end development seemed like the perfect next step.</p>
-			<p className="education">I earned my front-end web developer (FWD) certificate from BCIT in July of 2023. During the 7-month intensive program offered by BCIT I went from having almost no experience to making apps that called APIs! I started out small, making static websites as I learned to write HTML and CSS and progressed quickly to Sass, JavaScript/React, PHP, WordPress and e-commerce, specifically shopify and WooCommerce.</p>
-			<p className="interests">hobbies here pls</p>
-			<p className="languagues">HTML, CSS, JS/React, PHP, SQL</p>
-		</div>
-	);
-}
+// function PageAbout() {
+// 	return (
+// 		<div className="aboutMe">
+// 			<p className="personalBackground">After years of doing application support  -- for in house web apps and niche software -- I realized that I wanted to shift from supporting apps to making them. Once I started to look into jobs that I could see myself doing, front-end development seemed like the perfect next step.</p>
+// 			<p className="education">I earned my front-end web developer (FWD) certificate from BCIT in July of 2023. During the 7-month intensive program offered by BCIT I went from having almost no experience to making apps that called APIs! I started out small, making static websites as I learned to write HTML and CSS and progressed quickly to Sass, JavaScript/React, PHP, WordPress and e-commerce, specifically shopify and WooCommerce.</p>
+// 			<p className="interests">hobbies here pls</p>
+// 			<p className="languagues">HTML, CSS, JS/React, PHP, SQL</p>
+// 		</div>
+// 	);
+// }
 
-export default PageAbout;
-// TO DO: remove
+// export default PageAbout;
+// // TO DO: remove
